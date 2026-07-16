afd6558980caf1dc5166b87fb24725aeb3f99efe
Chitta Akshara <chittaakshara14@gmail.com>
2026-07-16 19:40:40 +0530

    Add files via upload

---
+++ Gates_using_MUX/xnor_gate.v
@@ -0,0 +1,41 @@
+//Design XNOR_gate using 2x1_MUX
+
+//2x1_MUX
+module mux_2x1(i0,i1,s,y);
+	input i0,i1,s;
+	output y;
+	assign y = (~s&i0)|(s&i1);
+endmodule
+
+
+
+//xnor gate using mux
+module xnor_gate(a,b,y);
+	input a,b;
+	output y;
+ mux_2x1 U1(.i0(~b),.i1(b),.s(a),.y(y));
+endmodule
+
+
+//TestBench
+module top;
+	reg a,b;
+	wire y;
+	xnor_gate dut(a,b,y);
+	initial begin
+		repeat(5)begin
+	    {a,b}=$random;
+    	#1;
+	    $display("a=%b b=%b y=%b",a,b,y);
+		end
+	end
+endmodule
+
+
+/*output
+# a=0 b=0 y=1
+# a=0 b=1 y=0
+# a=0 b=1 y=0
+# a=1 b=1 y=1
+# a=0 b=1 y=0
+*/
